2b4bb693ab1efcf0934c9396f15c22c4a0656ed1
mrfit1 <wbffiran@gmail.com>
2026-08-19 03:05:31 -0400

    Update sitemap URL in robots function

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,2 @@
 import type { MetadataRoute } from 'next';
-export default function robots(): MetadataRoute.Robots { return { rules:{userAgent:'*',allow:'/'}, sitemap:'https://example.com/sitemap.xml' }; }
+export default function robots(): MetadataRoute.Robots { return { rules:{userAgent:'*',allow:'/'}, sitemap:'https://hassangolestaneh.com/sitemap.xml' }; }
